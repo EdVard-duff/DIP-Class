516a09392f5d895a81733b4b4f2f03de007892d3
liuzhenchen <1059103811@qq.com>
2021-11-29 22:56:25 +0800

    start

---
+++ consistency_net.py
@@ -0,0 +1,18 @@
+from typing import ForwardRef
+import torch
+from torch import nn
+from torch._C import import_ir_module
+import torch.nn.functional as F
+import torchvision
+
+class SelfConsistNet(nn.Module):
+    def __init__(self,backbone_pretrained = True) -> None:
+        super(SelfConsistNet,self).__init__()
+        ## set backbone
+        resnet_layers = int
+    def forward():
+        pass
+
+
+if __name__ == '__main__':
+    pass
--- tmp.py
+++
@@ -1,3 +0,0 @@
-print('Hello world')
-
-import numpy as np
