ee67bd7aaa2a51c41fe285b5a6e65bebc4ea864c
liuzhenchen <1059103811@qq.com>
2021-12-04 19:13:54 +0800

    None

--- face-alignment/2d_landmark.py
+++ face-alignment/2d_landmark.py
@@ -1,9 +1,10 @@
 import face_alignment
 from skimage import io
-
+import numpy as np
 fa = face_alignment.FaceAlignment(face_alignment.LandmarksType._2D, flip_input=False,device='cuda:0',face_detector='blazeface') #face_detector: sfd,blazeface,dlib
 
-input = io.imread('I2G\man.jpeg')
+input = io.imread('I2G\man7.jpg')
 preds = fa.get_landmarks(input)
 ## preds = fa.get_landmarks_from_directory('Dirname')
 print(preds)
+np.save('I2G/man7.npy')
